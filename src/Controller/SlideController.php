45f8d7e3efb14342b894bb449efd959b77321a8f
Tibo <thibaut.desfours@gmail.com>
2020-04-23 14:04:40 +0200

    création slide

--- src/Controller/SlideController.php
+++ src/Controller/SlideController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Slide;
-use App\Entity\PictureEffect;
 use App\Entity\Picture;
+use App\Form\SlideType;
+use App\Entity\PictureEffect;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -39,6 +41,49 @@ class SlideController extends AbstractController
         return $this->render('slide/show.html.twig',[
             'slide' => $slide,
             'picture_effects' => $picture_effects,
+
+        ]);
+    }
+
+     /**
+     * @Route("slide/new", name="slideNew")
+     */
+    public function form(Slide $slide = null, Request $request, EntityManagerInterface $manager)
+    {
+        if(!$slide){
+            $slide = new Slide();
+        }
+
+        $form = $this->createForm(SlideType::class, $slide);
+        $form ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$slide->getId()){
+                $slide->setCreatedAt(new \Datetime());
+            }
+            $manager->persist($slide);
+            $manager->flush();
+
+            return $this->redirectToRoute('slideAdd',['id'=>$slide->getId()]);
+        }
+
+        return $this->render('slide/modalCreation.html.twig',[
+            'formSlide' => $form->createView()
+        ]);
+    }
+
+    
+    /**
+     * @Route("slide/add/{id}", name="slideAdd", requirements={"id"="\d+"})
+     */
+    public function AddPcitures(int $id, EntityManagerInterface $em):Response
+    {
+        $slide = $em ->getRepository(Slide::class)->find($id);
+        if($slide === null)
+            throw new NotFoundHttpException();
+
+        return $this->render('slide/add.html.twig',[
+            'slide' => $slide,
 
         ]);
     }
